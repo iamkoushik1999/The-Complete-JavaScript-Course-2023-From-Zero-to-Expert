36e53314529767ed26cdde2e4731232b395b5209
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-04-03 21:32:52 +0530

    10. Values and Variables

---
+++ JavaScript Fundamentals – Part 1/10_ValuesVariables.js
@@ -0,0 +1,25 @@
+let js = "Amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Koushik");
+console.log(23);
+
+let firstName = "Messi";
+
+console.log(firstName);
+
+let koushik_dutta = "KD";
+console.log(koushik_dutta);
+let sfunction = 10;
+console.log(sfunction);
+
+let person = "Koushik";
+console.log(person);
+let PI = 3.1415;
+console.log(PI);
+
+let myFirstJob = "Node Js Developer";
+let myCurrentJob = "Same";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
