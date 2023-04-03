62c5db30cd498d2f7b9a8a72f0205ca1a547ee3c
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-04-03 21:42:43 +0530

    12. Data Types

---
+++ JavaScript Fundamentals – Part 1/12_DataTypes.js
@@ -0,0 +1,24 @@
+// Data Types
+
+// Number : FLoating point numbers Used for decimal and integers
+let age = 23;
+console.log("let age = 23 ", typeof age);
+
+// String : Sequence of characters Used for text
+let firstName = "Koushik";
+console.log("let firstName = Koushik ", typeof firstName);
+
+// Boolean : Logical type that can only be true or false Used for taking decisions
+let fullAge = true;
+console.log("let fullAge = true ", typeof fullAge);
+
+// Undefined : Value taked by a variable that is not yet defined("empty value")
+let childed;
+console.log("let childed ", typeof childed);
+
+// Null : Also means "empty value"
+console.log("null ", typeof null);
+
+// Symbol(ES2015) : Value that is unique and cannot be changed [Not used for now]
+
+// BigInt(ES2020) : Larger integers than Number type can hold
