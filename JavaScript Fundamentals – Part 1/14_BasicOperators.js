f223ca7ea5885fed3b3787fb08c96eab80809b16
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-04-04 21:29:35 +0530

    14. Basic Operators

---
+++ JavaScript Fundamentals – Part 1/14_BasicOperators.js
@@ -0,0 +1,42 @@
+// Math Operators
+
+// const ageRam = 2023 - 1999;
+// const ageSham = 2023 - 2002;
+
+// console.log("Ram's age:", ageRam);
+// console.log("Sham's age:", ageSham);
+
+const now = 2023;
+const ageRam = now - 1999;
+const ageSham = now - 2002;
+
+console.log("Ram's age:", ageRam);
+console.log("Sham's age:", ageSham);
+
+console.log(ageRam * 2);
+console.log(ageSham / 2);
+console.log(2 ** 3);
+
+const firstName = "Koushik";
+const lastName = "Dutta";
+console.log(firstName + lastName);
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageRam > ageSham);
+console.log(ageRam >= 24);
+console.log(ageSham >= 24);
+
+const isFullAge = ageSham >= 24;
+console.log(isFullAge);
+
+console.log(ageRam > ageSham);
+console.log(now - 1999 > now - 2002);
