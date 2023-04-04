edb4e220e22db082e74d044c616ac10d7554c739
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-04-04 21:12:22 +0530

    13. let, const and var

---
+++ JavaScript Fundamentals – Part 1/13_let_const_var.js
@@ -0,0 +1,12 @@
+let age = 23;
+age = 24;
+
+const birthYear = 1999;
+// birthYear = 1999;
+// const job;
+
+var job = "Programmers";
+job = "Developer";
+
+lastName = "Dutta";
+console.log(lastName);
