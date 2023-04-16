af4eb31c82ee6fc79fdd42b0a7649205c3fae9c8
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-04-16 14:23:16 +0530

    42. Introduction to Objects

---
+++ JavaScript Fundamentals – Part 2/42_Introduction_Objects.js
@@ -0,0 +1,17 @@
+// Array
+const koushikArray = [
+  "koushik",
+  "dutta",
+  2023 - 1999,
+  "developer",
+  ["a", "b", "c"],
+];
+
+// object
+const koushik = {
+  firstName: "koushik",
+  lastName: "dutta",
+  age: 2023 - 1999,
+  job: "developer",
+  friends: ["a", "b", "c"],
+};
